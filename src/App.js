2e60909700534b1ecf810ab2f39e001af68fecc4
tushar0837 <tushar0837@gmail.com>
2025-11-09 20:43:40 +0530

    Fix React Router basename for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/catchingstars">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
